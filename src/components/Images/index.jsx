7db30c540e8a0a68b9e68fac68b289d3c9db4091
Bekzod <bekzodmahammadov627@gmail.com>
2024-01-10 13:59:15 +0500

    rasm joylandi

--- src/components/Images/index.jsx
+++ src/components/Images/index.jsx
@@ -14,44 +14,109 @@ import img12 from "../../assets/images/imgs/12.jpg";
 import img13 from "../../assets/images/imgs/13.jpg";
 import img14 from "../../assets/images/imgs/14.jpg";
 import img15 from "../../assets/images/imgs/15.jpg";
+import { useTranslation } from "react-i18next";
 
 function Images() {
+  const { t } = useTranslation();
   return (
     <div>
       <div className=" flex justify-center mt-[80px]">
         <div className="xl:w-[80%] w-[95%]">
           <div className="flex gap-10 items-center">
-            <h1 className="font-bold text-[18px] md:text-[23px] lg:text-[30px] h-1  md:h-3 xl:h-5 text-[#133466] w-[300px] uppercase">
-              Images
+            <h1 className="font-bold  text-[18px] md:text-[23px] lg:text-[28px] h-1  md:h-3 xl:h-5 text-[#133466] w-[400px] uppercase">
+              {t("image-title")} <br />
+              {t("image-title-2")}
             </h1>
             <p className="h-1 w-[90%] bg-[#FFC503] mt-6"></p>
           </div>
-          <div className="mt-10 hidden grid-cols-3 lg:grid">
-            <div className="d">
-              <img src={img1} alt="" />
-              <img src={img12} alt="" />
-              <img src={img3} alt="" />
-              <img src={img4} alt="" />
-              <img src={img15} alt="" />
+          {/* laptop */}
+          <div className="mt-[100px] hidden grid-cols-3 lg:grid">
+            <div className="d ">
+              <img
+                className="w-full object-cover h-[310px]"
+                src={img1}
+                alt=""
+              />
+              <img
+                className="w-full object-cover h-[500px]"
+                src={img12}
+                alt=""
+              />
+              <img
+                className="w-full object-cover h-[270px]"
+                src={img3}
+                alt=""
+              />
+              <img
+                className="w-full object-cover h-[400px]"
+                src={img4}
+                alt=""
+              />
+              <img
+                className="w-full object-cover h-[450px]"
+                src={img9}
+                alt=""
+              />
             </div>
             <div className="d">
-              <img src={img6} alt="" />
-              <img src={img7} alt="" />
-              <img src={img8} alt="" />
-              <img src={img9} alt="" />
-              <img src={img5} alt="" />
+              <img
+                className="w-full object-cover h-[280px]"
+                src={img11}
+                alt=""
+              />
+              <img
+                className="w-full object-cover h-[460px]"
+                src={img7}
+                alt=""
+              />
+              <img
+                className="w-full object-cover h-[330px]"
+                src={img8}
+                alt=""
+              />
+              <img
+                className="w-full object-cover h-[360px]"
+                src={img15}
+                alt=""
+              />
+              <img
+                className="w-full object-cover h-[500px]"
+                src={img2}
+                alt=""
+              />
             </div>
             <div className="d">
-              <img src={img11} alt="" />
-              <img src={img2} alt="" />
-              <img src={img13} alt="" />
-              <img src={img14} alt="" />
+              <img
+                className="w-full object-cover h-[490px]"
+                src={img13}
+                alt=""
+              />
+              <img
+                className="w-full object-cover h-[310px]"
+                src={img6}
+                alt=""
+              />
+              <img
+                className="w-full object-cover h-[280px]"
+                src={img5}
+                alt=""
+              />
+              <img
+                className="w-full object-cover h-[400px]"
+                src={img14}
+                alt=""
+              />
+              <img
+                className="w-full object-cover h-[450px]"
+                src={img10}
+                alt=""
+              />
+            </div>
+          </div>
 
-              <img src={img10} alt="" />
-            </div>
-          </div>
-          <div className="mt-10 hidden grid-cols-2 gap-1 md:grid lg:hidden">
-            <div className="grid grid-cols-1 gap-1">
+          {/* tablet */}
+          <div className="mt-[100px] hidden grid-cols-2 gap-2 md:grid lg:hidden">
+            <div className="grid grid-cols-1 gap-2">
               <img src={img1} alt="" />
               <img src={img12} alt="" />
               <img src={img3} alt="" />
@@ -71,7 +136,9 @@ function Images() {
               <img src={img10} alt="" />
             </div>
           </div>
-          <div className="mt-10 grid grid-cols-1 gap-1 md:hidden">
+
+          {/* mobile */}
+          <div className="mt-16 grid grid-cols-1 gap-2 md:hidden">
             <img src={img1} alt="" />
             <img src={img12} alt="" />
             <img src={img3} alt="" />
